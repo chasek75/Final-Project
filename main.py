91c22bbbcf7d7b541027d8491a93dd714b4d16fe
chasek75 <chase.kimball@maine.edu>
2024-12-15 21:32:46 -0500

    Enemy AI and loss condition

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from boardTools import *
 from inputProcess import *
+from Enemies import *
+import random
 
 def main():
     world = {}
@@ -11,13 +13,19 @@ def main():
         "x":5,
         "y":0
     }
+    enemy2loc = {
+        "x":10,
+        "y":10
+    }
     world["board"] = createBoard(15)
     world["playerloc"] = playerloc
     world["enemy1loc"] = enemy1loc
+    world["enemy2loc"] = enemy2loc
     world["playerName"] = getUserName()
     health = 100
+    
 
-    while True:
+    while health > 0:
         #Display Board and move user
         printBoard(world)
         userInput = getUserDir()
@@ -29,9 +37,41 @@ def main():
             playerloc["y"] -= 1
         if userInput == 's' and playerloc['y'] < len(world["board"])-1:
             playerloc["y"] += 1
-    
-        #HP tracking
+        #enemy 1 ai
+        enemy1xy = random.choice([0, 1])
+        if enemy1xy == 0:
+            if enemy1loc["x"] > playerloc["x"]:
+                enemy1loc["x"] -= 1
+            if enemy1loc["x"] < playerloc["x"]:
+                enemy1loc["x"] += 1
+        if enemy1xy == 1:
+            if enemy1loc["y"] > playerloc["y"]:
+                enemy1loc["y"] -= 1
+            if enemy1loc["y"] < playerloc["y"]:
+                enemy1loc["y"] += 1
+        #enemy 2 ai
+        enemy2xy = random.choice([0, 1])
+        if enemy2xy == 0:
+            if enemy2loc["x"] > playerloc["x"]:
+                enemy2loc["x"] -= 1
+            if enemy2loc["x"] < playerloc["x"]:
+                enemy2loc["x"] += 1
+        if enemy2xy == 1:
+            if enemy2loc["y"] > playerloc["y"]:
+                enemy2loc["y"] -= 1
+            if enemy2loc["y"] < playerloc["y"]:
+                enemy2loc["y"] += 1
+
+        #Enemy collision detector + print current health
+        if enemy1loc["x"] == playerloc["x"] and enemy1loc["y"] == playerloc["y"]:
+            health -= 10
+        if enemy2loc["x"] == playerloc["x"] and enemy2loc["y"] == playerloc["y"]:
+            health -= 10
         maxhp = str(health)
-        print("HP remaining:"+ maxhp +"/100")
+        if health > 0:
+            print(world["playerName"] + ":HP "+ maxhp +"/100")
+        else:
+            print("GAME OVER")
+
 
 main()
