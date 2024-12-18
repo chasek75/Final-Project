f991261afa917e54feff7d008b1fb034864fcef0
chasek75 <chase.kimball@maine.edu>
2024-12-18 18:56:55 -0500

    Initial Power ups

--- Enemies.py
+++ Enemies.py
@@ -7,10 +7,14 @@ def enemies():
     from main import playerloc
     from main import enemy1loc
     from main import enemy2loc
+    from main import Freeze
+
     #enemy 1 will do 1 of 3 options (move in x,y or not at all)
     enemy1xy = random.choice([0, 2])
+    if Freeze == True:
+        enemy1xy = 3
     #enemy1 moves in x direction towards playerlocation
-    if enemy1xy == 0:
+    if enemy1xy == 0 :
                 if enemy1loc["x"] > playerloc["x"]:
                     enemy1loc["x"] -= 1
                 if enemy1loc["x"] < playerloc["x"]:
@@ -20,7 +24,7 @@ def enemies():
                 if enemy1loc["x"] == playerloc["x"] and enemy1loc["y"] > playerloc["y"]:
                     enemy1loc["y"] -= 1
     #enemy1 moves in y direction towards playerlocation
-    if enemy1xy == 1:
+    if enemy1xy == 1 :
                 if enemy1loc["y"] > playerloc["y"]:
                     enemy1loc["y"] -= 1
                 if enemy1loc["y"] < playerloc["y"]:
@@ -30,33 +34,35 @@ def enemies():
                 if enemy1loc["y"] == playerloc["y"] and enemy1loc["x"] > playerloc["x"]:
                     enemy1loc["x"] -= 1
     #enemy1 wont move
-    if enemy1xy == 2:
+    if enemy1xy == 2 :
                 enemy1loc["x"] += 0
                     
     
     #enemy 2 player tracking
     #enemy2 will do 1 of 3 options (move in x,y or not at all)
     enemy2xy = random.choice([0, 2])
+    if Freeze == True:
+          enemy2xy = 2
     #enemy2 moves in x direction towards playerlocation
-    if enemy2xy == 0:
+    if enemy2xy == 0 :
                 if enemy2loc["x"] > playerloc["x"]:
-                    enemy2loc["x"] -= 1
+                    enemy2loc["x"] -= 2
                 if enemy2loc["x"] < playerloc["x"]:
-                    enemy2loc["x"] += 1
+                    enemy2loc["x"] += 2
                 if enemy2loc["x"] == playerloc["x"] and enemy2loc["y"] < playerloc["y"]:
-                    enemy2loc["y"] += 1
+                    enemy2loc["y"] += 2
                 if enemy2loc["x"] == playerloc["x"] and enemy2loc["y"] > playerloc["y"]:
-                    enemy2loc["y"] -= 1
+                    enemy2loc["y"] -= 2
     #enemy 2 moves in y direction towards playerlocation
-    if enemy2xy == 1:
+    if enemy2xy == 1 :
                 if enemy2loc["y"] > playerloc["y"]:
-                    enemy2loc["y"] -= 1
+                    enemy2loc["y"] -= 2
                 if enemy2loc["y"] < playerloc["y"]:
-                    enemy2loc["y"] += 1
+                    enemy2loc["y"] += 2
                 if enemy2loc["y"] == playerloc["y"] and enemy2loc["x"] < playerloc["x"]:
-                    enemy2loc["x"] += 1
+                    enemy2loc["x"] += 2
                 if enemy2loc["y"] == playerloc["y"] and enemy2loc["x"] > playerloc["x"]:
-                    enemy2loc["x"] -= 1
+                    enemy2loc["x"] -= 2
     #enemy2 wont move
-    if enemy2xy == 3:
+    if enemy2xy == 2 :
                 enemy2loc["x"] += 0
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def mainworld():
     #generate world dictionary and inputing board and locational data
     health = 3
     world = {}
+    #inputs location values
     global playerloc
     playerloc = {"x":0,"y":0}
     global enemy1loc
@@ -28,16 +29,28 @@ def mainworld():
     world["enemy2loc"] = enemy2loc
     world["freezeloc"] = freezeloc
     world["sheildloc"] = shieldloc
+    #get username from user and input into world dictionary
     userInput = getUserName()
     world["playerName"] = str(userInput)
     #sets initial score to 0
     scorecount = 0
     hp = health
+    #create power up variables to determine if power up are active and set them to false
+    global Freeze
+    Freeze = bool
+    Freeze = False
+    global Sheild
+    Sheild = bool
+    Sheild = True
+
     #game loop until health runs out
     while hp > 0:
-        #Display Board and move user
+        #Display Board and move user while health is above 0
         if hp > 0:
             printBoard(world)
+            #enemy player tracking function
+            enemies()
+            #get userinput and adjusts player location
             userInput = getUserDir()
             if userInput == 'd' and playerloc['x'] < len(world["board"])-1:
                 playerloc["x"] += 1
@@ -46,25 +59,31 @@ def mainworld():
             if userInput == 'w' and playerloc['y'] > 0:
                 playerloc["y"] -= 1
             if userInput == 's' and playerloc['y'] < len(world["board"])-1:
-                playerloc["y"] += 1
-        #enemy player tracking function
-            enemies()
+                playerloc["y"] += 1      
         #Enemy Player detector + print current health
-            if enemy1loc["x"] == playerloc["x"] and enemy1loc["y"] == playerloc["y"]:
-                hp -= 1
-            if enemy2loc["x"] == playerloc["x"] and enemy2loc["y"] == playerloc["y"]:
-                hp -= 1
+        #if shield is on no dmg is taken
+            if Sheild == False:
+                if enemy1loc["x"] == playerloc["x"] and enemy1loc["y"] == playerloc["y"]:
+                    hp -= 1
+                if enemy2loc["x"] == playerloc["x"] and enemy2loc["y"] == playerloc["y"]:
+                    hp -= 1
         #score incresed by 1 per turn
         scorecount += 1
         #convert hp to a string
         hpstring = str(hp)
-        #displays hp and player name at the top of the screen while alive
+        #displays hp in red and player name in default at the top of the screen while alive
         if hp >= 1:
             print(world["playerName"] + " : ",end="")
             print("\033[31m",end="")
-            print("HP "+ hpstring +"/3")
+            print("HP "+ hpstring +"/3",end="")
             print("\033[0m",end="")
-        #game over message after health hits 0
+            if Freeze == True:
+                print("\033[36m",end="")
+                print(" Freeze" + "\033[0m")
+                print("\033[0m",end="")
+            else:
+                print()
+        #red game over message and green score display after health hits 0
         else:
             print("\033[31m",end="")
             print("GAME OVER",end="")
